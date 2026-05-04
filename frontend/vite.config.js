e08de512cff9e63aae2d531c55874cb74d09e7ca
Franky Parcon <franky.parcon@globe.com.ph>
2026-05-04 10:38:54 +0800

    Fix: Vite config fix, clean build in Amplify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   },
   server: {
     port: 5173,
